475ab470ef87a44208395a01da2c9d26785077e5
leirelarreaeng <leirelarreaeng@gmail.com>
2024-07-05 23:39:36 +0100

    added sql injection validation

--- SlotBookingApp/Models/CalendarEventModelValidator.cs
+++ SlotBookingApp/Models/CalendarEventModelValidator.cs
@@ -6,11 +6,14 @@ public class CalendarEventModelValidator : AbstractValidator<CalendarEventModel>
     public CalendarEventModelValidator()
     {
         RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Please fill the Name");
+            .NotEmpty().WithMessage("Please fill the Name")
+            .NotNull().WithMessage("SecondName cannot be null")
+            .Matches("^[a-zA-Z\\s]*$");
 
         RuleFor(x => x.SecondName)
            .NotEmpty().WithMessage("Please fill the SecondName")
-           .NotNull().WithMessage("SecondName cannot be null");
+           .NotNull().WithMessage("SecondName cannot be null")
+           .Matches("^[a-zA-Z\\s]*$");
 
         RuleFor(x => x.Email)
            .NotEmpty().WithMessage("Please fill the Email")
@@ -24,10 +27,16 @@ public class CalendarEventModelValidator : AbstractValidator<CalendarEventModel>
 
         RuleFor(x => x.Comments)
           .NotEmpty().WithMessage("Please fill the Comments")
-          .NotNull().WithMessage("Comments cannot be null");
+          .NotNull().WithMessage("Comments cannot be null")
+          .Matches("^[a-zA-Z\\s]*$");
     }
 
     private bool BeAValidPhoneNumber(string phoneNumber)
+    {
+        return phoneNumber.All(char.IsDigit) && phoneNumber.Length >= 11 && phoneNumber.Length <= 15;
+    }
+
+    private bool BeAValidNameOrComment(string phoneNumber)
     {
         return phoneNumber.All(char.IsDigit) && phoneNumber.Length >= 11 && phoneNumber.Length <= 15;
     }
