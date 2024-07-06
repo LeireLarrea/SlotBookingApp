2c2b9ff6e8cba7fea5773e9a2fcde3b4a6f72f58
leirelarreaeng <leirelarreaeng@gmail.com>
2024-07-06 22:26:29 +0100

    improved frontend validation response

--- SlotBookingApp/Models/CalendarEventModelValidator.cs
+++ SlotBookingApp/Models/CalendarEventModelValidator.cs
@@ -7,28 +7,28 @@ public class CalendarEventModelValidator : AbstractValidator<CalendarEventModel>
     {
         RuleFor(x => x.Name)
             .NotEmpty().WithMessage("Please fill the Name")
-            .NotNull().WithMessage("SecondName cannot be null")
-            .Matches("^[a-zA-Z\\s]*$");
+            .NotNull().WithMessage("Name cannot be null")
+            .Matches("^[a-zA-Z\\s]*$").WithMessage("NAME can contain only letters and spaces");
 
         RuleFor(x => x.SecondName)
            .NotEmpty().WithMessage("Please fill the SecondName")
            .NotNull().WithMessage("SecondName cannot be null")
-           .Matches("^[a-zA-Z\\s]*$");
+           .Matches("^[a-zA-Z\\s]*$").WithMessage("SECOND NAME can contain only letters and spaces");
 
         RuleFor(x => x.Email)
            .NotEmpty().WithMessage("Please fill the Email")
            .NotNull().WithMessage("Email cannot be null")
-           .EmailAddress().WithMessage("Invalid email format.");
+           .EmailAddress().WithMessage("Invalid EMAIL format");
 
         RuleFor(x => x.Phone)
            .NotEmpty().WithMessage("Please fill the Phone")
            .NotNull().WithMessage("Phone cannot be null")
-           .Must(BeAValidPhoneNumber).WithMessage("Invalid mobile-phone number format.");
+           .Must(BeAValidPhoneNumber).WithMessage("You PHONE NUMBER must be between 11 - 15 numbers long");
 
         RuleFor(x => x.Comments)
           .NotEmpty().WithMessage("Please fill the Comments")
           .NotNull().WithMessage("Comments cannot be null")
-          .Matches("^[a-zA-Z\\s]*$");
+          .Matches("^[a-zA-Z\\s.,0-9]*$").WithMessage("COMMENTS can contain only letters, numbers, periods and commas");
     }
 
     private bool BeAValidPhoneNumber(string phoneNumber)
